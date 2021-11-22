58bee7a0f1255d682247d6028672936934fe2ca8
Captain Throwback <captainthrowback@hotmail.com>
2021-11-22 08:39:00 -0500

    Use prebuilt kernel and other cleanup
    Change-Id: Ia02bbcb6d86fd30915e6cd32b774e217a09bae31

--- omni_s3ve3g.mk
+++ omni_s3ve3g.mk
@@ -1,5 +1,5 @@
 # Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
